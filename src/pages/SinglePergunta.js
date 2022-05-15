ce36b596e40840bd0665a37600a565fa1d940c44
LeonelBR1 <105223064+LeonelBR1@users.noreply.github.com>
2022-05-15 17:48:46 -0400

    Estilo do componente resposta

--- src/pages/SinglePergunta.js
+++ src/pages/SinglePergunta.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import Header from '../components/Header'
+import Resposta from '../components/Resposta'
 
 const SinglePergunta = () => {
   const { id } = useParams()
   return (
     <div className="container">
       <Header />
-      <h1>{id}</h1>
+      <h1>{id}?</h1>
+      <div className="listaResposta">
+        <Resposta title="hoje é dia 32" autor="amanteigado" />
+        <Resposta title="hoje é dia 04" autor="luander gameplays" />
+        <Resposta title="onde eu peço uma pizza por aqui?" autor="calabresa" />
+      </div>
     </div>
   )
 }
