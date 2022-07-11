65f477da7d575f72e37ea43a7099145ccc4677fb
joao <joao.campos@unemat.br>
2022-07-11 12:06:42 -0500

    Adiciona configuração de interceptors para adiciona o token no header das requisições

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,14 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 })
 
+api.interceptors.request.use(
+  config => {
+    config.headers.use.Authorization = `${localStorage.getItem('faq@token')}`
+    return config
+  },
+  except => {
+    console.log(except)
+  }
+)
+
 export default api
